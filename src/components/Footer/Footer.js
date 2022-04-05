9b8f17b8d5958ffcfa678666d59e60c698b9662a
solaiman15-11026 <samimunna49@gmail.com>
2022-04-05 21:55:43 +0600

    or

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='bg-secondary text-white pb-2 pt-4 align-items-center'>
+        <div className='bg-dark text-white pb-2 pt-4 align-items-center'>
             <p>&copy; All Right Reserved Here || By Solaiman Sheik.</p>
         </div>
     );
