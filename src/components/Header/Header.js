b9d97cd67bc6faab3b97bfdaa0e4f7ed197dbefd
solaiman15-11026 <samimunna49@gmail.com>
2022-04-05 22:19:41 +0600

    done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,8 +8,8 @@ const Header = () => {
             <Link className='manubar-item' to='/'>Home</Link>
             <Link className='manubar-item' to='/review'>Review</Link>
             <Link className='manubar-item' to='/dashboard'>Dashboard</Link>
-            <Link className='manubar-item' to='/blog'>Blog</Link>
             <Link className='manubar-item' to='/about'>About</Link>
+            <Link className='manubar-item' to='/blog'>Blog</Link>
         </div>
     );
 };
