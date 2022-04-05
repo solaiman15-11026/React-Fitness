35be93b835eebfaac43448e276ec379e906ad4bc
solaiman15-11026 <samimunna49@gmail.com>
2022-04-05 22:34:45 +0600

    all

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,7 +12,7 @@ const About = () => {
                     <img className='img-fluid shadow-sm ' src={banner} alt="" />
                 </div>
                 <div className="banner-heading align-middle">
-                    <h1 className=' text-uppercase fw-bolder text-success fs-1 mb-2'>The Apple Watch</h1>
+                    <h1 className=' text-uppercase fw-bolder textt fs-1 mb-2'>The Apple Watch</h1>
                     <p className=' fw-bold mt-2 py-2 text' >Well, it’s a distinctive piece of tech that at first can be hard to get your head around, or even to quite know what it does. In short, it’s a watch that sends notifications and other information from your iPhone to your wrist and lets you send stuff back the other way, whether that’s a text message, a phone call or a simple way to call an Uber.</p>
                     <button className='btn btn-outline-success fs-5 mb-5'>Live Demo</button>
                 </div>
